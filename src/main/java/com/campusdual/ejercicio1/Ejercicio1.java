03a4515a0bd97cf674b492645385d6059bf7dac6
marta <105271643+ffanecaa@users.noreply.github.com>
2023-09-05 13:23:51 +0200

    inicio

--- src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
+++ src/main/java/com/campusdual/ejercicio1/Ejercicio1.java
@@ -1,7 +1,25 @@
 package com.campusdual.ejercicio1;
 
+
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
     public static void main(String[] args) {
-        System.out.println("hola" );
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Escriba un año");
+        Integer numero = teclado.nextInt();
+
+        if(numero%4==0){
+            if(numero % 100 == 0 && numero % 400 != 0){
+                System.out.println("El año no es bisiesto");
+            }else{
+                System.out.println("El año es bisiesto");
+            }
+        }else{
+            System.out.println("El año no es bisiesto");
+        }
     }
 }
+
+
